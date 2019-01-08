823e2c3757ab21f9ffb8edf94b05449e4591eaf7
Lon Ilesanmi <lonilesanmi@gmail.com>
2019-01-08 21:23:49 +0100

    Add route handler for setting a value

--- config/routes.js
+++ config/routes.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
   app.get('/', (_, res) => res.send({}));
   app.get('/cache/:key', cacheController.getItem);
   app.post('/cache/:key', cacheController.setItem);
-  app.put('/cache/:key', cacheController.updateItem);
+  app.put('/cache/:key', cacheController.setItem);
   app.get('/cache/keys', cacheController.getKeys);
   app.get('/cache/values', cacheController.getValues);
   app.delete('/cache/:key', cacheController.deleteItem);
--- controllers/cache.js
+++ controllers/cache.js
@@ -1,4 +1,5 @@
 const logger = require('morgan');
+const HttpStatus = require('http-status-codes');
 const CacheItem = require('../models/CacheItem');
 const utils = require('../libs/utils');
 
@@ -14,7 +15,7 @@ exports.getItem = (req, res, next) => {
       if (cachedItem) {
         logger.log('Cache hit');
 
-        res.send(cachedItem);
+        res.status(HttpStatus.OK).send(cachedItem);
       } else {
         logger.log('Cache miss');
         const value = utils.generateRandomString();
@@ -24,9 +25,47 @@ exports.getItem = (req, res, next) => {
           value,
         });
 
-        cacheItem.save().then(() => {
-          res.send(cacheItem);
-        }).catch(err => next(err));
+        cacheItem.save()
+          .then(() => {
+            res.status(HttpStatus.CREATED).send(cacheItem);
+          }).catch(err => next(err));
+      }
+    }).catch(err => next(err));
+};
+
+/**
+ * POST /cache
+ * Add a item in the cached
+ */
+exports.setItem = (req, res, next) => {
+  req.assert('value', 'Error - value - cannot be blank').notEmpty();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    logger.error(errors);
+
+    res.status(HttpStatus.BAD_REQUEST).json(errors);
+  }
+
+  const { key, } = req.params;
+  const { value, } = req.body;
+
+  CacheItem.findOne({ key, })
+    .then((cachedItem) => {
+      if (cachedItem) {
+        cachedItem.value = value;
+        res.status(HttpStatus.OK).send(cachedItem);
+      } else {
+        const cacheItem = new CacheItem({
+          key,
+          value,
+        });
+
+        cacheItem.save()
+          .then(() => {
+            res.send(cacheItem);
+          }).catch(err => next(err));
       }
     }).catch(err => next(err));
 };
