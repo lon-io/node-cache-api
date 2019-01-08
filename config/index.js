a7bbc0d1e64cff99ee49c715113d052fb2dd9c71
Lon Ilesanmi <lonilesanmi@gmail.com>
2019-01-08 23:51:27 +0100

    Optimize CacheItem pre-save hook

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  CACHE_SIZE_LIMIT: 10,
+  CACHE_SIZE_LIMIT: 3,
   DEFAULT_TTL: 3600, // 1 hour
 };
--- models/CacheItem.js
+++ models/CacheItem.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('../config');
 const utils = require('../libs/utils');
+const logger = require('../libs/logger');
 
 const cacheItemSchema = new mongoose.Schema({
   key: { type: String, unique: true },
@@ -18,15 +19,29 @@ cacheItemSchema.pre('save', function save(next) {
   // Set the expiry timestamp
   if (item.ttl) item.expires = utils.getExpiryTimestamp(item.ttl);
 
-  /* eslint-disable no-use-before-define */
-  CacheItem.count()
-    .then((numberOfItems) => {
-      if (numberOfItems >= config.CACHE_SIZE_LIMIT) {
-        // Delete the oldest item
-        CacheItem.findOneAndRemove({}, {}, { sort: { created_at: 1 } })
-          .then(() => next());
-      } else next();
-    });
+  // Check for limit if the item is new
+  if (item.isNew) {
+    /* eslint-disable no-use-before-define */
+    CacheItem.countDocuments()
+      .then((numberOfItems) => {
+        console.log('numberOfItems');
+        console.log('numberOfItems');
+        console.log('numberOfItems');
+        console.log('numberOfItems');
+        console.log('numberOfItems');
+        if (numberOfItems >= config.CACHE_SIZE_LIMIT) {
+          logger.info('Maximum exceeded, removing oldest item...');
+
+          // Delete the oldest item
+          CacheItem.findOne({}, {}, { sort: { created_at: 1 } })
+            .then((hit) => {
+              hit
+                .remove()
+                .then(() => next());
+            });
+        } else next();
+      });
+  }
   /* eslint-enable no-use-before-define */
 });
 
